c670ec90067731217f50ab4c19cceb276215bf5c
Italo Matos <italomatossouza@hotmail.com>
2024-02-13 21:15:09 -0300

    [ADD] added new colors

--- lib/src/core/const/colors.dart
+++ lib/src/core/const/colors.dart
@@ -1,38 +1,121 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static get light => _Light();
-  static get dark => _Dark();
+  static InternalAppColors get light => _Light();
+  static InternalAppColors get dark => _Dark();
 }
 
-class _Light {
-  // Background ====================================================================================
-  final Color backgroundColor = const Color(0xFFFFFFFF);
+abstract class InternalAppColors {
+  Color get backgroundColor;
+  Color get defaultTextColor;
+  Color get defaultLightTextColor;
+  Color get defaultLightTitleTextColor;
+  Color get defaultLightButtonTextColor;
+  Color get defaultButtonColor;
+  Color get defaultLightButtonColor;
+  Color get defaultDarkButtonColor;
+  Color get selectedSell;
+  Color get unselectedCell;
+  Color get wrongCell;
 
-  // Text ==========================================================================================
-  final Color defaultTextColor = const Color(0xFF3D475D);
-  final Color defaultLightTextColor = const Color(0xFF5b67ca);
-  final Color defaultLightTitleTextColor = const Color(0xFFFFFFFF);
-  final Color defaultLightButtonTextColor = const Color(0xFFFFFFFF);
+  Color get wrongNumberText;
+  Color get presetedNumberText;
+  Color get selectedNumberText;
+  Color get definedByUserNumberText;
 
-  // Buttons =======================================================================================
-  final Color defaultButtonColor = const Color(0xFF5B67CA);
-  final Color defaultLightButtonColor = const Color(0xFFF6F6F6);
-  final Color defaultDarkButtonColor = const Color(0xFF353a50);
+  Color get sameRowOrColumn;
 }
 
-class _Dark {
-  // Background ====================================================================================
-  final Color backgroundColor = const Color(0xFF2b2e43);
+class _Light extends InternalAppColors {
+  // Background ================1====================================================================
+  @override
+  Color get backgroundColor => const Color(0xFFFFFFFF);
 
   // Text ==========================================================================================
-  final Color defaultTextColor = const Color(0xFFFFFFFF);
-  final Color defaultLightTitleTextColor = const Color(0xFFFFFFFF);
-  final Color defaultLightTextColor = const Color(0xFF3f4255);
-  final Color defaultLightButtonTextColor = const Color(0xFFFFFFFF);
+  @override
+  Color get defaultTextColor => const Color(0xFF3D475D);
+  @override
+  Color get defaultLightTextColor => const Color(0xFF5b67ca);
+  @override
+  Color get defaultLightTitleTextColor => const Color(0xFFFFFFFF);
+  @override
+  Color get defaultLightButtonTextColor => const Color(0xFFFFFFFF);
 
   // Buttons =======================================================================================
-  final Color defaultButtonColor = const Color(0xFF353b63);
-  final Color defaultDarkButtonColor = const Color(0xFF353a50);
-  final Color defaultLightButtonColor = const Color(0xFFFFFFFF);
+  @override
+  Color get defaultButtonColor => const Color(0xFF5B67CA);
+  @override
+  Color get defaultLightButtonColor => const Color(0xFFF6F6F6);
+  @override
+  Color get defaultDarkButtonColor => const Color(0xFF353a50);
+
+  // Cell ==========================================================================================
+  @override
+  Color get selectedSell => const Color(0xFF5b67ca);
+  @override
+  Color get unselectedCell => const Color(0xFFffffff);
+  @override
+  Color get wrongCell => const Color(0xFFfecad8);
+
+  @override
+  Color get selectedNumberText => const Color(0xFFffffff);
+
+  @override
+  Color get presetedNumberText => const Color(0xFF6b7383);
+
+  @override
+  Color get wrongNumberText => const Color(0xFFf5637f);
+
+  @override
+  Color get definedByUserNumberText => const Color(0xFFbbbfc6);
+
+  @override
+  Color get sameRowOrColumn => const Color(0xFFeaecff);
+}
+
+class _Dark extends InternalAppColors {
+  // Background ====================================================================================
+  @override
+  Color get backgroundColor => const Color(0xFF2b2e43);
+
+  // Text ==========================================================================================
+  @override
+  Color get defaultTextColor => const Color(0xFFFFFFFF);
+  @override
+  Color get defaultLightTitleTextColor => const Color(0xFFFFFFFF);
+  @override
+  Color get defaultLightTextColor => const Color(0xFF3f4255);
+  @override
+  Color get defaultLightButtonTextColor => const Color(0xFFFFFFFF);
+
+  // Buttons =======================================================================================
+  @override
+  Color get defaultButtonColor => const Color(0xFF353b63);
+  @override
+  Color get defaultDarkButtonColor => const Color(0xFF353a50);
+  @override
+  Color get defaultLightButtonColor => const Color(0xFFFFFFFF);
+
+  // Cell ==========================================================================================
+  @override
+  Color get selectedSell => const Color(0xFF5a66c9);
+  @override
+  Color get unselectedCell => const Color(0xFF353a50);
+  @override
+  Color get wrongCell => const Color(0xFF673755);
+
+  @override
+  Color get selectedNumberText => const Color(0xFFffffff);
+
+  @override
+  Color get presetedNumberText => const Color(0xFFffffff);
+
+  @override
+  Color get wrongNumberText => const Color(0xFFf45170);
+
+  @override
+  Color get definedByUserNumberText => const Color(0xFF7f8290);
+
+  @override
+  Color get sameRowOrColumn => const Color(0xFF474c61);
 }
