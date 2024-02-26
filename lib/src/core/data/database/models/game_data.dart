ab6754f9be2a0102b9d7284c66ab0604f2985a57
Italo Matos <italomatossouza@hotmail.com>
2024-02-26 17:07:17 -0300

    [ADD] created game data model

---
+++ lib/src/core/data/database/models/game_data.dart
@@ -0,0 +1,58 @@
+import 'package:sudoku/src/core/game/engine/models/grid.dart';
+import 'package:sudoku/src/core/game/engine/models/pontuation.dart';
+
+class GameData {
+  GameData({
+    required Pontuation pontuation,
+    required int errorCount,
+    required Grid playableGrid,
+    required Grid completedGrid,
+    required Map<int, int> userInputPositions,
+    required Map<String, bool> correctlyFilledPositions,
+  })  : _pontuation = pontuation,
+        _errorCount = errorCount,
+        _completedGameGrid = completedGrid,
+        _playableGameGrid = playableGrid,
+        _userInputPositions = userInputPositions,
+        _correctlyFilledPositions = correctlyFilledPositions;
+
+  final Pontuation? _pontuation;
+  final int? _errorCount;
+
+  final Grid? _playableGameGrid;
+  final Grid? _completedGameGrid;
+
+  final Map<int, int>? _userInputPositions;
+  final Map<String, bool>? _correctlyFilledPositions;
+
+  factory GameData.fromJSON(Map<String, dynamic> json) {
+    return GameData(
+      pontuation: Pontuation(json['pontuation']),
+      errorCount: json['error_count'],
+      playableGrid: Grid(json['playable_grid']),
+      completedGrid: Grid(json['completed_grid']),
+      userInputPositions: json['user_input_positions'],
+      correctlyFilledPositions: json['correctly_filled_positions'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'pontuation': _pontuation?.pontuation,
+      'error_count': _errorCount,
+      'playable_grid': _playableGameGrid?.matrix,
+      'completed_grid': _completedGameGrid?.matrix,
+      'user_input_positions': _userInputPositions,
+      'correctly_filled_positions': _correctlyFilledPositions,
+    };
+  }
+
+  Pontuation get pontuation => _pontuation ?? Pontuation(0);
+  int get errorCount => _errorCount ?? 0;
+
+  Map<int, int> get userInputPositions => _userInputPositions ?? {};
+  Map<String, bool> get correctlyFilledPositions => _correctlyFilledPositions ?? {};
+
+  Grid get playableGrid => _playableGameGrid ?? Grid.empty();
+  Grid get completeGrid => _completedGameGrid ?? Grid.empty();
+}
---
+++ lib/src/core/data/database/models/player_data.dart
@@ -0,0 +1,41 @@
+import 'package:sudoku/src/core/data/database/models/game_data.dart';
+
+class PlayerData {
+  PlayerData({
+    String? userID,
+    String? username,
+    GameData? lastGameData,
+    List<GameData>? allGameData,
+  })  : _userID = userID,
+        _username = username,
+        _allGameData = allGameData,
+        _lastGameData = lastGameData;
+
+  final String? _userID;
+  final String? _username;
+  final List<GameData>? _allGameData;
+  final GameData? _lastGameData;
+
+  factory PlayerData.fromJson(Map<String, dynamic> json) {
+    return PlayerData(
+      allGameData: List.generate(json['all_game_data']?.lenght ?? 0, (index) => GameData.fromJSON(json['all_game_data']?[index])),
+      lastGameData: json['last_game_data'],
+      userID: json['user_id'],
+      username: json['username'],
+    );
+  }
+
+  Map<String, dynamic> toJSON() {
+    return {
+      'all_game_data': _allGameData?.map((e) => e.toJson()).toList(),
+      'last_game_data': _lastGameData?.toJson(),
+      'user_id': _userID,
+      'username': _username,
+    };
+  }
+
+  String? get userID => _userID;
+  String? get userName => _username;
+  GameData? get lastGameData => _lastGameData;
+  List<GameData>? get allGameData => _allGameData;
+}
